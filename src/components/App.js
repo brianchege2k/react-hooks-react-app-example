f22beee5897842b0f7a4930f625eb18eb5d6f0cd
brianchege2k <chegebrian84@gmail.com>
2024-05-02 15:20:00 +0300

    lab done

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ function App() {
     <div className="App">
       <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
       <p className="App-intro">
+        <h1>Welcome to React!</h1>
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
         HTML, and also allows us to add in components, which are separate,
